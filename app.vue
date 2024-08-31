80f89636c78f48d3470b8cc5b70179c7eb02ec78
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-08-31 15:59:34 +0200

    feat: Add navigation menu to app.vue

--- app.vue
+++ app.vue
@@ -1,5 +1,21 @@
 <template>
   <NuxtLayout>
+    <nav>
+      <ul>
+        <li>
+          <NuxtLink to='/'>Home</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to='/about'>About</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to='/services'>Services</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to='/contact'>Contact</NuxtLink>
+        </li>
+      </ul>
+    </nav>
     <NuxtPage />
   </NuxtLayout>
 </template>
