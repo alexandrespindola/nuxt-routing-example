5d0d0401e1d6c8e9dbe8401aba39d2487162e89c
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-09-01 21:00:11 +0200

    feat: Update NoteService to use relative API endpoint
    This commit updates the NoteService class to use a relative API endpoint instead of a hardcoded one. It improves the flexibility and maintainability of the code by allowing the API base URI to be easily changed.

--- services/NoteService.ts
+++ services/NoteService.ts
@@ -1,12 +1,12 @@
 export default class NoteService {
-  private base_uri: string = "/api/notes";
-
-  public async getNote() {
-    const uri = this.base_uri;
-    const rawResponse = await fetch(uri, {
-      method: "GET",
-    })
-    const response = await rawResponse.json();
-    return response;
+    private base_uri: string = "/api";
+  
+    public async getNote() {
+      const uri = `${this.base_uri}/notes`;
+      const rawResponse = await fetch(uri, {
+        method: "GET",
+      });
+      const response = await rawResponse.json();
+      return response;
+    }
   }
-}
