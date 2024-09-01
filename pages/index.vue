8ac696b6761d527de682f29c304baa674c2d00d3
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-09-01 20:48:54 +0200

    feat: Add lazy loading for images and fetch note from API

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,33 @@
 <template>
     <div>
         <h1>Home View</h1>
+        <p>This is the home view.</p>
+        <h2>Title: {{ note.title }}</h2>
+        <p>Content: {{ note.content }}</p>
     </div>
 </template>
 
 <script lang="ts" setup>
+import NoteService from '../services/NoteService';
+import { ref, type Ref } from 'vue';
+
+// Definir a interface para o objeto note
+interface Note {
+  title: string;
+  content: string;
+}
+
+// Tipar o ref com a interface Note
+const note: Ref<Note> = ref({
+  title: 'loading...',
+  content: 'loading...'
+});
+
+const noteService = new NoteService();
+
+onMounted(async () => {
+  note.value = await noteService.getNote();
+});
 </script>
 
 <style scoped lang="scss">
---
+++ services/NoteService.ts
@@ -0,0 +1,12 @@
+export default class NoteService {
+  private base_uri: string = "http://localhost:3000/api/notes";
+
+  public async getNote() {
+    const uri = this.base_uri;
+    const rawResponse = await fetch(uri, {
+      method: "GET",
+    })
+    const response = await rawResponse.json();
+    return response;
+  }
+}
